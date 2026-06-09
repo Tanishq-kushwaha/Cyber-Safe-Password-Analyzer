82946aa531373e46ef25c5f1f39e29a0a288ad9a
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-09 12:37:21 +0530

    fix(DOM): Correct regex for symbols and update event listener to log input value accurately

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// target Section 
 const passwordInput = document.querySelector("#password-input");
-
 const strengthBar = document.querySelector("#strength-bar");
 const ruleLength = document.querySelector("#rule-length");
 const ruleUppercase = document.querySelector("#rule-uppercase");
@@ -8,7 +8,14 @@ const ruleNumber = document.querySelector("#rule-number");
 const ruleSymbol = document.querySelector("#rule-symbol");
 
 
+// Rejex section
+const regexUppercase = /[A-Z]/;
+const regexLowercase = /[a-z]/;
+const regexNumber = /[0-9]/;
+const regexSymbol = /[!@#$%^&*(),.?":<>|{}]/;
 
-passwordInput.addEventListener("input", function(){
-       console.log(passwordInput.innerText);
+
+// Event
+passwordInput.addEventListener("input", function(event){
+       console.log(event.target.value);
 });
