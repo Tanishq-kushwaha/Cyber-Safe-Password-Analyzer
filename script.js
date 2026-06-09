7550124072b6414062d83b612d51bfdfe7af9c25
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-09 11:42:56 +0530

    refactor(DOM): Rename input variable for clarity and organize rule selectors

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let input = document.querySelector("input");
+let passwordInput = document.querySelector("input");
 
+let strengthBar = document.querySelector("#strength-bar");
+
+
+let ruleLength = document.querySelector("#rule-length");
+let ruleUppercase = document.querySelector("#rule-uppercase");
+let ruleLowercase = document.querySelector("#rule-lowercase");
+let ruleNumber = document.querySelector("#rule-number");
+let ruleSymbol = document.querySelector("#rule-symbol");
