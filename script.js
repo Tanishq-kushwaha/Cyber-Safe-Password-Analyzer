379a890941edc1f27738ecd0a2edb1d3e950cd84
Tanishq Kushwaha <tanishqkushwaha09@gmail.com>
2026-06-09 12:08:30 +0530

    fix(DOM): Update input event listener to log password input value correctly

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const ruleUppercase = document.querySelector("#rule-uppercase");
 const ruleLowercase = document.querySelector("#rule-lowercase");
 const ruleNumber = document.querySelector("#rule-number");
 const ruleSymbol = document.querySelector("#rule-symbol");
+
+
+
+passwordInput.addEventListener("input", function(){
+       console.log(passwordInput.innerText);
+});
